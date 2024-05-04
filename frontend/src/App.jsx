7ff498f4520ddc418fb7481414af1d1a4d12467b
kishore28kumar <kishore28kks@gmail.com>
2024-05-04 09:04:38 +0530

    Initial Setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,8 @@
+import Home from "./page/Home";
+// import Result from "./page/Result";
+
+function App() {
+  return <Home />;
+}
+
+export default App;
---
+++ frontend/src/page/Home.jsx
@@ -0,0 +1,10 @@
+import Navbar from "../components/Navbar";
+// import Hero from "../components/Hero";
+// import Content from "../components/Content";
+// import Footer from "../components/Footer";
+
+const Home = () => {
+  return <Navbar />;
+};
+
+export default Home;
